ff5bc78308fe0ecb5a162160c282791b9443f4ca
neshaolivia <neshapramitha32@gmail.com>
2025-12-18 13:02:58 +0700

    Add models and complete database migrations

---
+++ database/migrations/2025_12_18_050459_create_favorites_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('favorites', function (Blueprint $table) {
+            $table->id();
+
+            $table->foreignId('id_user')
+                  ->constrained('users')   // ✅ BUKAN id_users
+                  ->cascadeOnDelete();
+
+            $table->foreignId('id_buku')
+                  ->constrained('buku')
+                  ->cascadeOnDelete();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('favorites');
+    }
+};
